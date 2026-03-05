d093511a27c29aa9484999cf56a10086b4542cff
kurt <kurt@erdmann-freunde.de>
2026-03-05 17:54:37 +0100

    Add no-releases deploy recipe

---
+++ recipe/deploy/no_releases.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Deployer;
+
+use function Deployer\Support\str_contains;
+
+desc('Preparing host for deploy without releases');
+task('deploy:prepare', function () {
+    // Check if shell is POSIX-compliant
+    $result = run('echo $0');
+
+    if (!str_contains($result, 'bash') && !str_contains($result, 'sh')) {
+        throw new \RuntimeException('Shell on your server is not POSIX-compliant. Please change to sh, bash or similar.');
+    }
+
+    run('if [ ! -d {{deploy_path}} ]; then mkdir -p {{deploy_path}}; fi');
+
+    // Create metadata .dep dir.
+    run('cd {{deploy_path}} && if [ ! -d .dep ]; then mkdir .dep; fi');
+})->hidden();
+
+set('release_path', static function () {
+    return get('deploy_path');
+});
+
+task('deploy:release', static function () {
+    // No release folder is created.
+})->hidden();
+
+task('deploy:shared', static function () {
+    // Shared links are not required without releases.
+})->hidden();
+
+task('deploy:symlink', static function () {
+    // Symlink switching is not required without releases.
+})->hidden();
+
+task('deploy:cleanup', static function () {
+    // Cleanup of old releases is not required without releases.
+})->hidden();
+
+task('rollback', static function () {
+    // Rollback through previous releases is not possible without releases.
+});
